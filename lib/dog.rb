4aa776fcb8ee3f5ba48d4cb22e67b117b1d3645c
Nathaniel Adam <iammanyadams@gmail.com>
2020-03-19 09:42:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-  # code goes here
+
+@@all = []
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  def initialize(name, owner)
+    @name =name
+    @owner =owner
+    @mood ="nervous"
+    @@all << self
+  end
+
+  def get_adopted(owner_name)
+    self.owner = owner_name
+  end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,50 @@
 class Owner
-  # code goes here
+attr_reader :name, :species
+
+@@all = []
+
+def initialize(name, species)
+  @name = name
+  @species = "human"
+  @@all << self
+end
+  def say_species
+      puts "I am a #{self.species}."
+  end
+  def self.all
+    @@all
+  end
+  def count
+    @cat.owner.count + @dog.owner.count
+  end
+  def reset_all
+    self.all.clear
+  end
+
+
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+  end
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  def buy_dog(name)
+    Dog.new(name, self)
+  def walk_dogs
+    self.dogs.each {|dogs| dog.mood = "happy"}
+  end
+  def feed_cats
+    self.cats.each {|cats| cat.mood = "happy"}
+  end
+
+def list_pets
+  puts "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+end
+
+
+  end
 end
