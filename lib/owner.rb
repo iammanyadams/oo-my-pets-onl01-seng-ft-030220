e0351f5dc62c6fd25546c78128978e637ca2f02b
Nathaniel Adam <iammanyadams@gmail.com>
2020-03-19 09:44:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,17 @@
 class Owner
   
-  
+  class Owner
+attr_reader :name, :species
+
+@@all = []
+
+def initialize(name, species)
+  @name = name
+  @species = "human"
+  @@all << self
+end
+
+
   
   
   
